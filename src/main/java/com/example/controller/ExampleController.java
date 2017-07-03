db7a8bb7bf7436df6676c8d318841d19b3da2dc6
simpleag <1013194733@qq.com>
2017-07-03 09:26:22 +0800

    model

--- src/main/java/com/example/controller/ExampleController.java
+++ src/main/java/com/example/controller/ExampleController.java
@@ -5,7 +5,9 @@ import com.example.service.ExampleService;
 import com.example.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -48,5 +50,15 @@ public class ExampleController {
     public Result logout(HttpSession session) {
         session.invalidate();
         return Result.success();
+    }
+    @PostMapping(value = "/upload")
+    public @ResponseBody String loadExcel(@RequestParam("file") MultipartFile file){
+        if (file == null){
+            System.out.print("empty");
+            return "wrong";
+        }else{
+            System.out.print(file.getOriginalFilename());
+            return "ok";
+        }
     }
 }
